5bce928c07848b5d2f46d5541a830e094550c4fd
sophianx234 <dx4336969@gmail.com>
2026-08-14 16:54:25 +0000

    style: reduce banner vertical padding on mobile

--- src/components/features/ui/Banner.tsx
+++ src/components/features/ui/Banner.tsx
@@ -1,6 +1,6 @@
 function Banner() {
     return (
-        <div className="py-6 bg-zinc-900">
+        <div className="py-2 md:py-6 bg-zinc-900">
 
         <div className=" flex text-white items-center justify-center font-chewy py-2">
            <h1 className="text-lg sm:text-4xl tracking-wide">
